ecda3d857f844e1c29323b67e97ebd05be7529e3
Yénekpeya Tuo <tuohabibroland@gmail.com>
2024-11-16 01:09:42 +0000

    Initial deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('hello')
-})
+// Serve les fichiers statiques de React depuis le dossier 'dist'
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
-app.listen(port, ()=>{
-    console.log(`le serveur tourne sur le port ${port}`)
-})
+// Route par défaut pour envoyer l'application React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
+});
+
+// Démarre le serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
